c242c86b8827a1a043d85acfb4cafbcf58964828
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-12-23 16:39:42 +0900

    update

--- src/train_lgbm.py
+++ src/train_lgbm.py
@@ -181,4 +181,4 @@ if __name__ == "__main__":
     submission_file_name = "../output/submission.csv"
     oof_file_name = "../output/oof_lgbm.csv"
     with timer("Full model run"):
-        main(debug=True,use_pkl=False)
+        main(debug=False,use_pkl=False)

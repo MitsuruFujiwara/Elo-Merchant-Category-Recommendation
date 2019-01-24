629f906048eb0eb52d5ceb00544a279c39db7856
MitsuruFujiwara <fujiwara52jp@gmail.com>
2019-01-25 06:52:43 +0900

    LB#75

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ from multiprocessing import Pool, cpu_count
 from sklearn.metrics import mean_squared_error
 from time import time, sleep
 
-NUM_FOLDS = 11
+NUM_FOLDS = 5
 
 #feats_drop = pd.read_csv('feats_drop.csv')['feature'].tolist()
 

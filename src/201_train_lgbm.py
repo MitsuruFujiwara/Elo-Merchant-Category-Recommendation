340f9f2b11edd909ee6f666ef2e9a4c8c09450de
MitsuruFujiwara <fujiwara52jp@gmail.com>
2019-02-16 23:59:28 +0900

    LB#124

--- src/201_train_lgbm.py
+++ src/201_train_lgbm.py
@@ -183,6 +183,6 @@ def main(debug=False):
 if __name__ == "__main__":
     submission_file_name = "../output/submission_lgbm.csv"
     oof_file_name = "../output/oof_lgbm.csv"
-    configs = json.load(open('../configs/201_lgbm.json'))
+    configs = json.load(open('../configs/207_lgbm_best.json'))
     with timer("Full model run"):
         main(debug=False)
